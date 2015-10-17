5cc9b79d95c5742ba5ac3558b497ea4c6376753a
Thanakom sangnetra <loki.top11@gmail.com>
2015-10-17 14:22:00 +0700

    Update Config.php

--- src/Config.php
+++ src/Config.php
@@ -7,11 +7,11 @@ class Config {
 
     const PAYPAL_MODE = 'sandbox';
     const PAYPAL_ENDPOINT = 'https://api.sandbox.paypal.com';
-    const PAYPAL_CLIENT_ID = 'Afin354O1EuZmQ6IxUvymQisjgSPH9TPqegw0vUejuKPjklvhCgr_6t9KyJtpVJzfPUwsUvRXUXUclI_';
-    const PAYPAL_CLIENT_SECRET = 'EJ52jmjLJQSLZxjeihYe9wn0c0FhbxBNELvbML2kVCXzqFpY4_rFw2qhmQ5N2qdDaeA-fnBkyhSFF-ix';
+    const PAYPAL_CLIENT_ID = '<ID>';
+    const PAYPAL_CLIENT_SECRET = '<SECRET>';
 
     const BRAINTREE_MODE = 'sandbox';
-    const BRAINTREE_MERCHANT_ID = 'g9y369x3qz7yp94v';
-    const BRAINTREE_PUBLIC_KEY = 't3kfxvt522kpvdhz';
-    const BRAINTREE_PRIVATE_KEY = 'a566c0e6bdcfa4ceb5052e87a0bfad8a';
+    const BRAINTREE_MERCHANT_ID = '<MERCHANT';
+    const BRAINTREE_PUBLIC_KEY = 'PUBLIC_KEY';
+    const BRAINTREE_PRIVATE_KEY = 'PRIVATE_KEY';
 }
